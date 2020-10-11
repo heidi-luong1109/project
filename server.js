a77a3285b319ec57efd64875b4b21cd28d503aa8
heidi-luong1109 <heidi.luong1109@yahoo.com>
2020-10-11 08:28:20 +0000

    Add https

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var express = require("express");
-
+const https = require('https');
 var app = express();
-
+const fs = require('fs');
+const port = 8081;
 
 app.use(express.static('public'));
-
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 
-var server = app.listen(8081, function(){
-    var port = server.address().port;
+//https cert PATH
+var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
+
+
+var server = https.createServer(options, app);
+
+server.listen(port, function(){
     console.log("Server started at http://localhost:%s", port);
 });
+
+
